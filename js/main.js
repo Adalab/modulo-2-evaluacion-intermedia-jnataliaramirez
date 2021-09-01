1b87bd4317b2ceefabd9501a48e00cbb353ebcb9
J. Natalia Ramirez <jnataliaramirez@gmail.com>
2021-09-01 10:37:03 +0200

    Create function for count

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 'use strict';
 
+// * Variables
 const number = document.querySelector('.js_number');
 const submit = document.querySelector('.js_submit');
+
+let counter = 1;
+
 
 // * Generar un numero aleatorio
 function getRandomNumber(max) {
@@ -13,11 +17,17 @@ let randomNumber = getRandomNumber(100);
 
 console.log(`Mi número aleatorio es ${randomNumber}`);
 
+// * Contador
+function count() {
+    return counter++
+}
+
+
 // * Cojer value de la usuaria 
 //función del listener
 function getUserNumber() {
     let inputValue = number.value;
-    console.log(inputValue)
+    count()
     return inputValue;
 }
 
@@ -28,7 +38,6 @@ submit.addEventListener('click', getUserNumber);
 // let userNumber = getUserNumber();
 
 // * Identificar si es un número
-
 function isANumber(number) {
     if (!isNaN(number)) {
         if (number > 0 && number <= 100) {
@@ -43,7 +52,6 @@ function isANumber(number) {
 
 
 // * Identificar si es el número ingresado es mayor o menor al número aleatorio
-
 function getMajorOrMinor(number) {
     if (number > randomNumber ) {
         return 'Demasiado alto.' 
@@ -53,6 +61,7 @@ function getMajorOrMinor(number) {
         return 'Has ganado campeona'
     }
 }
+
 
 
 
