07b269c9928e5fee4a5647e614b5f8ac87d736fd
J. Natalia Ramirez <jnataliaramirez@gmail.com>
2021-09-13 15:43:22 +0200

    Fix improvements

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const number = document.querySelector('.js_number');
 const submit = document.querySelector('.js_submit');
 const clue = document.querySelector('.js_clue');
 const tries = document.querySelector('.js_try');
-let counter =0;
+let counter = 0;
 
 
 // * Generar un numero aleatorio
@@ -13,85 +13,52 @@ function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 }
 
-//Variable del nuúmero aleatorio
+// * Variable del nuúmero aleatorio
 let randomNumber = getRandomNumber(100);
 
 console.log(`Mi número aleatorio es ${randomNumber}`);
 
-// // * Contador
-// function count() {
-//     return counter++
-// }
-
-
-// * Cojer value de la usuaria 
-//función del listener
-function getUserNumber() {
-    let inputValue = number.value;
-    // count()
-    return inputValue;
-}
-
-// Listener
-// submit.addEventListener('click', getUserNumber);
-
-//Variable del número introducido
-// let userNumber = getUserNumber();
 
 // * Identificar si es un número
-function isANumber(number) {
+function validNumber(number) {
+    let varReturn;
+
     if (!isNaN(number)) {
         if (number > 0 && number <= 100) {
-            return true
-        } else {
-            return 'El número debe estar entre 1 y 100.'
-        }
+            if (number > randomNumber) {
+                varReturn = 'Demasiado alto.'
+            } else if (number < randomNumber) {
+                varReturn = 'Demasiado bajo.'
+            } else if (number === randomNumber) {
+                varReturn = '¡Has ganado campeona!'
+            }
+        } else if (number <= 0 || number >= 100) {
+            varReturn = 'El número debe estar entre 1 y 100.'
+        } 
     } else {
-        return 'El valor a ingresar debe ser un número';
+        varReturn = 'No es valido ingresar texto'
     }
-}
 
-
-// * Identificar si es el número ingresado es mayor o menor al número aleatorio
-function getMajorOrMinor(number) {
-    if (number > randomNumber ) {
-        return 'Demasiado alto.' 
-    } else if (number < randomNumber){
-        return 'Demasiado bajo.'
-    } else {
-        return 'Has ganado campeona'
-        console.log('afa')
-    }
+    return varReturn
 }
 
 // * Escribir en pantalla la pista 
-function writeClue(a) {
-    // let majorOrMinor = getMajorOrMinor()
-    return clue.innerHTML = a
+function writeClue(something) {
+    return clue.innerHTML = something;
 }
 
 // * Escribir en pantalla cuantos intentos va 
-function writeTry() {
-    // let triesCount = count()
-    return tries.innerHTML = counter
+function writeTry(counter) {
+    return tries.innerHTML = counter;
 }
 
 // * Juego 
-
-function game() {
-    counter++;
-    let userNumber = getUserNumber();
-    let is = isANumber(userNumber);
-    let clue = getMajorOrMinor(userNumber);
+function game(event) {
+    let inputValue = parseInt(number.value);
+    let clue = validNumber(inputValue);
     writeClue(clue);
-    writeTry();
+    writeTry(counter++);
+    event.preventDefault();
 }
 
 submit.addEventListener('click', game);
-
-
-
-
-
-
-
